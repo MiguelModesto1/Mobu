6ac97011db12f6e97c7468f06da0e1bf396dcb1d
MiguelModesto1 <101589792+MiguelModesto1@users.noreply.github.com>
2023-03-04 23:11:54 +0000

    Adição do projeto para o Mobu

---
+++ mobu_backend/mobu_backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mobu_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
